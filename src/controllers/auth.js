89147a40a54ac20f0d6e3edeeb4e0532ca86cdf0
Vladislav-Pavlenko <151663067+Vladislav-Pavlenko@users.noreply.github.com>
2024-09-27 19:59:42 +0300

    fix error

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -72,8 +72,8 @@ export const signoutController = async (req, res) => {
 export const requestResetEmailController = async (req, res) => {
   await requestResetToken(req.body.email);
   res.json({
-    message: 'Reset password email was successfully sent!',
     status: 200,
+    message: 'Reset password email has been successfully sent!',
     data: {},
   });
 };
@@ -81,8 +81,8 @@ export const requestResetEmailController = async (req, res) => {
 export const resetPasswordController = async (req, res) => {
   await resetPassword(req.body);
   res.json({
-    message: 'Password was successfully reset!',
     status: 200,
+    message: 'Password has been successfully reset!',
     data: {},
   });
 };
